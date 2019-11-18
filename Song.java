48aecac3b198f7493e3269e5446adc1925dbfd03
rmhm1 <49662821+rmhm1@users.noreply.github.com>
2019-11-18 18:16:13 -0500

    Add files via upload

--- Song.java
+++ Song.java
@@ -1,7 +1,4 @@
-/**
- * 
- */
-package musicVisualization;
+package prj5;
 
 /**
  * @author Saakshi Naveen
@@ -11,73 +8,119 @@ public class Song {
     private String name;
     private String artist;
     private int date;
+    private String genre;
+
+
     /**
      * constructor that creates new song
-     * @param n name of song
-     * @param a artist of song
-     * @param d date of song release
+     * 
+     * @param n
+     *            name of song
+     * @param a
+     *            artist of song
+     * @param d
+     *            date of song release
      */
-    public Song(String n, String a, int d)
-    {
-        name = n;
+    public Song(String title, String a, int d, String genre) {
+        name = title;
         artist = a;
         date = d;
+        this.genre = genre;
     }
+
+
     /**
      * gets date
+     * 
      * @return date
      */
-    public int getDate()
-    {
+    public int getDate() {
         return date;
     }
+
+
     /**
      * sets date
+     * 
      * @param newDate
+     *            new Date to set
      */
-    public void setDate(int newDate)
-    {
+    public void setDate(int newDate) {
         date = newDate;
     }
+
+
     /**
      * gets name
+     * 
      * @return name
      */
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
+
+
     /**
      * sets name
+     * 
      * @param newName
+     *            new Song name
      */
-    public void setName(String newName)
-    {
-        name= newName;
+    public void setName(String newName) {
+        name = newName;
     }
+
+
     /**
      * gets artist
+     * 
      * @return artist
      */
-    public String getArtist()
-    {
+    public String getArtist() {
         return artist;
     }
+
+
     /**
      * sets artist
+     * 
      * @param newArtist
+     *            The new artist
      */
-    public void setArtist(String newArtist)
-    {
+    public void setArtist(String newArtist) {
         artist = newArtist;
     }
+
+
+    /**
+     * Gets the genre of a song
+     * 
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+
+    /**
+     * Sets a new genre
+     * 
+     * @param newGenre
+     *            the new genre
+     */
+    public void setGenre(String newGenre) {
+        genre = newGenre;
+    }
+
+
     /**
      * checks to see if this song is equal to another song
-     * @param other object
+     * 
+     * @param other
+     *            object
      * @return true if equals
      */
-    public boolean equals(Object obj)
-    {
+    public boolean equals(Object obj) {
         if (obj == this) {
             return true;
         }
@@ -88,8 +131,8 @@ public class Song {
             return false;
         }
         Song other = (Song)obj;
-        if (other.getName().equals(name) && other.getDate() == date && other.getArtist().equals(artist))
-        {
+        if (other.getName().equals(name) && other.getDate() == date && other
+            .getArtist().equals(artist)) {
             return true;
         }
         return false;
