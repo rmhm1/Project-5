234717e47d644970745a82eee96a5dcd663885b6
emiswan <57882587+emiswan@users.noreply.github.com>
2019-11-19 16:28:50 -0500

    Add files via upload

--- GlyphCalculator.java
+++ GlyphCalculator.java
@@ -36,54 +36,153 @@ public class GlyphCalculator {
         Iterator<Glyph> iter = glyphs.iterator();
         while (iter.hasNext()) {
             Glyph current = iter.next();
-            Iterator<Attributes> fans = current.getFans().iterator();
-            int[] newFans = new int[4];
-            while (fans.hasNext()) {
-                Attributes attributes = fans.next();
-                HobbyEnum hobby = attributes.getHobby();
-                switch (hobby) {
-                    case READ:
-                        newFans[0] = newFans[0]++;
-                        break;
-                    case ART:
-                        newFans[1] = newFans[1]++;
-                        break;
-                    case SPORTS:
-                        newFans[2] = newFans[2]++;
-                        break;
-                    case MUSIC:
-                        newFans[3] = newFans[3]++;
-                        break;
-                }
-                current.setFanBars(newFans);
-                Iterator<Attributes> listeners = current.getListeners()
-                    .iterator();
-                int[] newListeners = new int[4];
-                while (listeners.hasNext()) {
-                    attributes = listeners.next();
-                    hobby = attributes.getHobby();
-                    switch (hobby) {
-                        case READ:
-                            newListeners[0] = newListeners[0]++;
-                            break;
-                        case ART:
-                            newListeners[1] = newListeners[1]++;
-                            break;
-                        case SPORTS:
-                            newListeners[2] = newListeners[2]++;
-                            break;
-                        case MUSIC:
-                            newListeners[3] = newListeners[3]++;
-                            break;
-                    }
-                    current.setListenerBars(newListeners);
-                }
-
-            }
+            LinkedList<Attributes> fans = current.getFans();
+            int[] newFans = calculateHobbyBars(fans);
+            current.setFanBars(newFans);
+            LinkedList<Attributes> listeners = current.getListeners();
+            int[] newListeners = calculateHobbyBars(listeners);
+            current.setListenerBars(newListeners);
         }
         return glyphs;
     }
+    
+    /**
+     * represents by region
+     */
+    public LinkedList<Glyph> byRegion() {
+        Iterator<Glyph> iter = glyphs.iterator();
+        while (iter.hasNext()) {
+            Glyph current = iter.next();
+            LinkedList<Attributes> fans = current.getFans();
+            int[] newFans = calculateRegionBars(fans);
+            current.setFanBars(newFans);
+            LinkedList<Attributes> listeners = current.getListeners();
+            int[] newListeners = calculateRegionBars(listeners);
+            current.setListenerBars(newListeners);
+        }
+        return glyphs;
+    }
+    
+    /**
+     * represents by major.
+     */
+    public LinkedList<Glyph> byMajor() {
+        Iterator<Glyph> iter = glyphs.iterator();
+        while (iter.hasNext()) {
+            Glyph current = iter.next();
+            LinkedList<Attributes> fans = current.getFans();
+            int[] newFans = calculateMajorBars(fans);
+            current.setFanBars(newFans);
+            LinkedList<Attributes> listeners = current.getListeners();
+            int[] newListeners = calculateMajorBars(listeners);
+            current.setListenerBars(newListeners);
+        }
+        return glyphs;
+    }
+    
+    
 
+    
+    /**
+     * builds an int array based on hobby for a list of
+     * student attributes.
+     * @param attributes
+     *              A LinkedList of attributes to build an int array for.
+     */
+    private int[] calculateHobbyBars(LinkedList<Attributes> attributes)
+    {
+        Iterator<Attributes> iter  = attributes.iterator();
+        int[] newBars = new int[4];
+        while (iter.hasNext()) {
+            Attributes current = iter.next();
+            HobbyEnum hobby = current.getHobby();
+            switch (hobby) {
+                case READ:
+                    newBars[0] = newBars[0]++;
+                    break;
+                case ART:
+                    newBars[1] = newBars[1]++;
+                    break;
+                case SPORTS:
+                    newBars[2] = newBars[2]++;
+                    break;
+                case MUSIC:
+                    newBars[3] = newBars[3]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+            return newBars;
+    }
+    
+    /**
+     * builds an int array based on region for a list of
+     * student attributes.
+     * @param attributes
+     *              A LinkedList of attributes to build an int array for.
+     */
+    private int[] calculateRegionBars(LinkedList<Attributes> attributes)
+    {
+        Iterator<Attributes> iter  = attributes.iterator();
+        int[] newBars = new int[4];
+        while (iter.hasNext()) {
+            Attributes current = iter.next();
+            RegionEnum region = current.getRegion();
+            switch (region) {
+                case NE_USA:
+                    newBars[0] = newBars[0]++;
+                    break;
+                case SE_USA:
+                    newBars[1] = newBars[1]++;
+                    break;
+                case OTHER_USA:
+                    newBars[2] = newBars[2]++;
+                    break;
+                case OUTSIDE_USA:
+                    newBars[3] = newBars[3]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+            return newBars;
+    }
+    
+    
+    /**
+     * builds an int array based on major for a list of
+     * student attributes.
+     * @param attributes
+     *              A LinkedList of attributes to build an int array for.
+     */
+    private int[] calculateMajorBars(LinkedList<Attributes> attributes)
+    {
+        Iterator<Attributes> iter  = attributes.iterator();
+        int[] newBars = new int[4];
+        while (iter.hasNext()) {
+            Attributes current = iter.next();
+            MajorEnum major = current.getMajor();
+            switch (major) {
+                case CMDA_MATH:
+                    newBars[0] = newBars[0]++;
+                    break;
+                case CS:
+                    newBars[1] = newBars[1]++;
+                    break;
+                case ENG_OTHER:
+                    newBars[2] = newBars[2]++;
+                    break;
+                case OTHER:
+                    newBars[3] = newBars[3]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+            return newBars;
+    }
+    
 
     /**
      * sorts by title
@@ -93,10 +192,7 @@ public class GlyphCalculator {
             for (int j = i; (j > 0) && glyphs.getEntry(j).getSong().getName()
                 .compareTo(glyphs.getEntry(j - 1).getSong()
                     .getName()) < 0; j--) {
-                Glyph g = glyphs.getEntry(j - 1);
-                Glyph g2 = glyphs.getEntry(j);
-                glyphs.replace(j, g);
-                glyphs.replace(j - 1, g2);
+                glyphs.swap(j, j - 1);
             }
         }
         return glyphs;
@@ -111,10 +207,7 @@ public class GlyphCalculator {
             for (int j = i; (j > 0) && glyphs.getEntry(j).getSong().getGenre()
                 .compareTo(glyphs.getEntry(j - 1).getSong()
                     .getGenre()) < 0; j--) {
-                Glyph g = glyphs.getEntry(j - 1);
-                Glyph g2 = glyphs.getEntry(j);
-                glyphs.replace(j, g);
-                glyphs.replace(j - 1, g2);
+                glyphs.swap(j, j - 1);
             }
         }
         return glyphs;
